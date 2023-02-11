8f8feee4d21d5814af48271fe6a41f0f7ac973d8
degro <seryoga.githab@gmail.com>
2023-02-11 20:21:43 +0200

    add Home Work

---
+++ DomRabota2/src/Main.java
@@ -0,0 +1,69 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        byte b = 45;
+        short s = 346;
+        int countBread = 578;
+        long countVariable = 785623948569834L;
+        int countDog = 9;
+
+        String st = "Stroka";
+
+        char cr = 'r';
+        char cr1 = 'y';
+
+        Integer in = 10;
+
+        float f1 = 3.14F;
+
+//        System.out.println(b);
+//        System.out.println(s);
+//        System.out.println(countBread);
+//        System.out.println(countVariable);
+//        System.out.println(countDog);
+//        System.out.println(st);
+//        System.out.println(cr);
+//        System.out.println(cr1);
+//        System.out.println(in);
+//        System.out.println(f1);
+
+
+        System.out.println("\t\t\t\tДобро пожаловать в калькулятор" +
+                " сложения чисел");
+
+        System.out.println("Введите первое число");
+        int a = new Scanner(System.in).nextInt();
+
+        System.out.println("Введите второе число");
+        double a1 = new Scanner(System.in).nextDouble();
+
+        System.out.println(a + "+" + a1 + "=" + (a + a1));
+
+
+//        System.out.println("\t\t\t\tДобро пожаловать в калькулятор" +
+//                " вычитания чисел");
+//
+//        System.out.println("Введите первое число");
+        //     double a = new Scanner(System.in).nextDouble();
+//
+//        System.out.println("Введите второе число");
+        //       double a1 = new Scanner(System.in).nextDouble();
+//
+//        System.out.println(a + "-" + a1 + "=" + (a - a1));
+//
+
+
+//                System.out.println("\t\t\t\tДобро пожаловать в калькулятор" +
+//                " деления чисел");
+//
+//        System.out.println("Введите первое число");
+//        double a = new Scanner(System.in).nextDouble();
+//
+//        System.out.println("Введите второе число");
+//        double a1 = new Scanner(System.in).nextDouble();
+//
+//        System.out.println(a + "/" + a1 + "=" + (a / a1));
+
+    }
+}
